2fe3394894e201b4f9d766fdc3b728b0322a8545
HaEun <mjcho0130@gmail.com>
2025-03-25 12:43:45 +0900

    bankv1 login logout complete

--- src/main/java/com/metacoding/bankv1/user/UserController.java
+++ src/main/java/com/metacoding/bankv1/user/UserController.java
@@ -1,5 +1,6 @@
 package com.metacoding.bankv1.user;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
     private final UserService userService;
+    private final HttpSession session;
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    @PostMapping("/login") // 로그인만 비밀번호가 걸려있어서 @Post로 던진다. @Get이 아님, 원래는 select라서 @Get인데 예외다.
+    public String login(UserRequest.LoginDTO loginDTO) {
+        User sessionUser = userService.로그인(loginDTO);
+        session.setAttribute("sessionUser", sessionUser);
+        return "redirect:/";
+    }
 
     @GetMapping("/login-form")
     public String loginForm() {
+        session.setAttribute("metacoding", "apple");
         return "user/login-form";
     }
 
     @GetMapping("/join-form")
     public String joinForm() {
+        String value = (String) session.getAttribute("metacoding");
+        System.out.println("value : " + value);
         return "user/join-form";
     }
 
